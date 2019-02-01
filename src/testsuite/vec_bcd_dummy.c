ee63226c0f81f65f75d3006a6706c2ab2e9d6f27
Steven Munroe <munroesj52@gmail.com>
2019-02-01 14:47:15 -0600

    Cleanup arith128_test_bcd.c and vec_bcd_dummy.c. Use vBCD_t type and fix formatting and white-space. Add compile test for BCD and some DFP builtins.
    	* src/testsuite/arith128_test_bcd.c: Remove arith128.h include.
    	(db_vec_BC10ts2i128): Formatting and white-space.
    	(test_48): Ditto.
    	(test_cvtbcd2c100): Use vBCD_t. Formatting and white-space.
    	(test_bcd_addsub): Ditto.
    	(test_bcd_muldiv): Ditto.
    
    	* src/testsuite/vec_bcd_dummy.c (test_pack_Decimal128,
    	test_pack_Decimal128_cast, test_unpack_Decimal128,
    	test_unpack_Decimal128_cast, test_vec_BCD2DFP,
    	test_vec_DFP2BCD): New functions.
    	([__GNUC__ > 4] test__builtin_ddedpdq, test__builtin_denbcdq):
    	New functions.
    	([__GNUC__ > 4 [_ARCH_PWR8]] test__builtin_bcdadd,
    	test__builtin_bcdadd_eq, test__builtin_bcdsub,
    	test__builtin_bcdsub_eq, test__builtin_bcdmax,
    	test__builtin_bcdabsd): New functions.
    
    Signed-off-by: Steven Munroe <munroesj52@gmail.com>

--- src/testsuite/vec_bcd_dummy.c
+++ src/testsuite/vec_bcd_dummy.c
@@ -8,6 +8,42 @@
 #include <vec_common_ppc.h>
 #include <vec_bcd_ppc.h>
 
+vf64_t
+test_pack_Decimal128 (_Decimal128 lval)
+{
+  return vec_pack_Decimal128 (lval);
+}
+
+vui32_t
+test_pack_Decimal128_cast (_Decimal128 lval)
+{
+  return (vui32_t) vec_pack_Decimal128 (lval);
+}
+
+_Decimal128
+test_unpack_Decimal128 (vf64_t lval)
+{
+  return vec_unpack_Decimal128 (lval);
+}
+
+_Decimal128
+test_unpack_Decimal128_cast (vui32_t lval)
+{
+  return vec_unpack_Decimal128 ((vf64_t) lval);
+}
+
+_Decimal128
+test_vec_BCD2DFP (vui32_t val)
+{
+  return vec_BCD2DFP (val);
+}
+
+vui32_t
+test_vec_DFP2BCD (_Decimal128 val)
+{
+  return vec_DFP2BCD (val);
+}
+
 vui8_t
 test_vec_bcdctb100s (vui8_t a)
 {
@@ -37,3 +73,68 @@ test_vec_bcddiv (vui32_t a, vui32_t b)
 {
   return vec_bcddiv (a, b);
 }
+
+#if (__GNUC__ > 4)
+_Decimal128
+test__builtin_ddedpdq ( _Decimal128 value)
+{
+  return __builtin_ddedpdq (2, value);
+}
+
+_Decimal128
+test__builtin_denbcdq ( _Decimal128 value)
+{
+  return __builtin_denbcdq (1, value);
+}
+
+#ifdef _ARCH_PWR8
+vi128_t
+test__builtin_bcdadd (vi128_t vra, vi128_t vrb)
+{
+  return __builtin_bcdadd (vra, vrb, 0);
+}
+
+int
+test__builtin_bcdadd_eq (vi128_t vra, vi128_t vrb)
+{
+  return __builtin_bcdadd_eq (vra, vrb, 0);
+}
+
+vi128_t
+test__builtin_bcdsub (vi128_t vra, vi128_t vrb)
+{
+  return __builtin_bcdsub (vra, vrb, 0);
+}
+
+int
+test__builtin_bcdsub_eq (vi128_t vra, vi128_t vrb)
+{
+  return __builtin_bcdsub_eq (vra, vrb, 0);
+}
+
+vi128_t
+test__builtin_bcdmax (vi128_t vra, vi128_t vrb)
+{
+  vi128_t result = vra;
+  if (__builtin_bcdsub_lt (vra, vrb, 0))
+    result = vrb;
+
+  return result;
+}
+
+vi128_t
+test__builtin_bcdabsd (vi128_t vra, vi128_t vrb)
+  {
+    //vi128_t vra_mb,  vrb_ma;
+    vi128_t result;
+    //vra_mb = __builtin_bcdsub (vra, vrb, 0);
+    //vrb_ma = __builtin_bcdsub (vrb, vra, 0);
+    if (__builtin_bcdsub_gt (vra, vrb, 0))
+    result = __builtin_bcdsub (vra, vrb, 0);
+    else
+    result = __builtin_bcdsub (vrb, vra, 0);
+
+    return result;
+  }
+#endif
+#endif

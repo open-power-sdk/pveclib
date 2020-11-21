e50ff94845afacc5d8bff039009b4aae75523cf1
Steven Munroe <munroesj52@gmail.com>
2020-11-21 15:47:41 -0600

    Initial support for Gather/Scatter ops, 1st pass for f32.
    This is the Single Precision Float variant of Gather/Scatter.
    The Gathers have three vector result forms:
    - Float single words expanded to Float Double as vector double.
    - Float single words as vector float.
    
    The doubleword results take signed doubleword offsets/indexes.
    The word results take signed word offsets/indexes.
    The Scatter has two vector source forms;
    - Vector Double rounded and converted to float single stored as words.
    - Vector Float where all 4 words are stored are float single.
    
    Doubleword sources take vector signed doubleword offsets/indexes,
    while word sources take vector signed word offsets/indexes,
    There are also forms where offsets are provided as 2/4 const scalar
    offsets. These are optimized for const offsets but are also used
    in the implementation of the vector offset/index forms.
    
    	* src/pveclib/vec_f32_ppc.h [@cond INTERNAL]: Forward declare;
    	vec_vglfsso, vec_vlxsspx, vec_vsstfsso, vec_vstxsspx;
    	(vec_vgl4fsso, vec_vgl4fswo, vec_vgl4fswsx, vec_vgl4fswx,
    	vec_vglfsdo, vec_vglfsdsx, vec_vglfsdx, vec_vglfsso,
    	vec_vlxsspx):  New inline operations.
    	(vec_vsst4fsso, vec_vsst4fswo, vec_vsst4fswsx, vec_vsst4fswx,
    	vec_vsstfsso, vec_vsstfsdsx, vec_vsstfsdx, vec_vsstfsso,
    	vec_vstxsspx):  New inline operations.
    
    	* src/testsuite/arith128_test_f32.c (test_f32):
    	New test array.
    	(test_lvgfsx): New unit test functions.
    	(test_stf32): New test array.
    	(test_stvgfsx): New unit test functions.
    	(matrix_f32): New matrix for column vector tests.
    	(test_f32_Imatrix_init, test_f32_Imatrix_check,
    	test_f32_matrix_transpose, test_f32_matrix_gather_transpose,
    	test_f32_matrix_gatherx2_transpose,
    	test_f32_matrix_gatherx4_transpose): New unit/perf tests.
    	(test_indentity_array): New unit test.
    	(test_time_f32): Add timed tests for timed_scalar_f32_transpose,
    	test_f32_matrix_gather_transpose,
    	test_f32_matrix_gatherx2_transpose,
    	test_f32_matrix_gatherx4_transpose.
    	(test_vec_f32): Add test_lvgdfdx, test_stvgdfdx,
    	test_indentity_array calls to unit test driver.
    
    	* src/testsuite/arith128_test_f32.h (matrix_f32,
    	test_f32_Imatrix_init, test_f32_Imatrix_check,
    	test_f32_matrix_transpose, test_f32_matrix_gather_transpose,
    	test_f32_matrix_gatherx2_transpose,
    	test_f32_matrix_gatherx4_transpose): New externs.
    
    	* src/testsuite/vec_f32_dummy.c
    	[ (__GNUC__ > 7) && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)]:
    	New compile test test_vec_pack_dpsp.
    	(test_vec_vsst4fsso test_vec_vsstfswwo, test_vec_vsst4fswsx,
    	test_vec_vsst4fswx): New compile tests.
    	(test_vec_vgl4fswsx, test_vec_vgl4fswx, test_vec_vgl4fswo,
    	test_vec_vgl4fsso): New compile tests.
    	(test_vec_vsstfsdsx, test_vec_vsstfsdx, test_vec_vsstfsdo,
    	test_vec_vsstfsso, test_vec_vsstfsso_032): New compile tests.
    	(test_vec_vglfsdsx, test_vec_vglfsdx, test_vec_vglfsdo,
    	test_vec_vglfsso, test_vec_vglfsso_032): New compile tests.
    	(test_vec_vlxsspx test_vec_vlxsspx_c0, test_vec_vlxsspx_c1,
    	test_vec_vlxsspx_c2, test_vec_vlxsspx_c3, test_vec_vlxsspx_c4,
    	test_vlxsspx_v0): New compile tests.
    	(test_vstxsspx, test_vstxsspx_c0, test_vstxsspx_c1,
    	test_vstxsspx_c2, test_vstxsspx_c3, test_vstxsspx_c4,
    	test_vstxsspx_v1): New compile tests.
    	[__ORDER_BIG_ENDIAN__} (test_vlxsspx_v1, test_vglssfso_v2):
    	(test_veclfgux_v1, test_veclfgux_v2): New compile tests.
    	[__GNUC__ && (__GNUC__ > 7) && defined(_ARCH_PWR8)]
    	(test_veclfgux_v3): New compile test.
    
    	* src/testsuite/vec_perf_f32.c: Include
    	<testsuite/arith128_test_f32.h>.
    	(timed_scalar_f32_transpose, timed_gather_f32_transpose,
    	timed_gatherx2_f32_transpose, timed_gatherx4_f32_transpose):
    	New timed test functions.
    
    	* src/testsuite/vec_perf_f32.h (timed_scalar_f32_transpose,
    	timed_gather_f32_transpose, timed_gatherx2_f32_transpose,
    	timed_gatherx4_f32_transpose): New externs.
    
    Signed-off-by: Steven Munroe <munroesj52@gmail.com>

--- src/testsuite/arith128_test_f32.c
+++ src/testsuite/arith128_test_f32.c
@@ -1796,6 +1796,514 @@ test_float_iszero (void)
 #define __DEBUG_PRINT__ 1
 #undef __DEBUG_PRINT__
 
+static float test_f32[] =
+    {
+	0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+	8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0,
+	0.0, -1.0, -2.0, -3.0, -4.0, -5.0, -6.0, -7.0,
+	-8.0, -9.0, -10.0, -11.0, -12.0, -13.0, -14.0, -15.0
+    };
+
+int
+test_lvgfsx (void)
+{
+  vi32_t i1;
+  vf32_t e, j;
+  vi64_t id1;
+  vf64_t ed;
+  vf64_t jd, jd0, jd1;
+  int rc = 0;
+
+  printf ("\ntest_Vector Gather-Load Single Float\n");
+
+  ed =  (vf64_t) CONST_VINT64_DW ( 0.0, -2.0);
+  jd0 = vec_vlxsspx (0, test_f32);
+  jd =  (vf64_t) vec_permdi ((vui64_t) jd0, (vui64_t)ed, 1);
+
+  rc += check_v2f64 ("vec_vlxsspx 1:", (vf64_t) jd, (vf64_t) ed);
+
+  ed =  (vf64_t) CONST_VINT64_DW ( 1.0, -2.0 );
+  jd0 = vec_vlxsspx (4, test_f32);
+  jd =  (vf64_t) vec_permdi ((vui64_t) jd0, (vui64_t)ed, 1);
+
+  rc += check_v2f64 ("vec_vlxsspx 2:", (vf64_t) jd, (vf64_t) ed);
+
+  ed =  (vf64_t) CONST_VINT64_DW ( 15.0, -2.0 );
+  jd0 = vec_vlxsspx (60, test_f32);
+  jd =  (vf64_t) vec_permdi ((vui64_t) jd0, (vui64_t)ed, 1);
+
+  rc += check_v2f64 ("vec_vlxsspx 3:", (vf64_t) jd, (vf64_t) ed);
+
+  i1 = (vi32_t) { 4, 60, 8, 56 };
+  ed =  (vf64_t) CONST_VINT64_DW ( 1.0, -2.0 );
+  jd0 = vec_vlxsspx (i1[0], test_f32);
+  jd =  (vf64_t) vec_permdi ((vui64_t) jd0, (vui64_t)ed, 1);
+
+  rc += check_v2f64 ("vec_vlxsspx 4:", (vf64_t) jd, (vf64_t) ed);
+
+  ed = (vf64_t) CONST_VINT64_DW ( 15.0, -2.0 );
+  jd1 = vec_vlxsspx (i1[1], test_f32);
+  jd = (vf64_t) vec_permdi ((vui64_t) jd1, (vui64_t)ed, 1);
+
+  rc += check_v2f64 ("vec_vlxsspx 5:", (vf64_t) jd, (vf64_t) ed);
+
+  // This test depends on the merge of scalars from lxsiwzx 4/5
+
+  ed =  (vf64_t) { 1.0, 15.0 };
+#if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+  jd = (vf64_t) vec_permdi ((vui64_t) jd1, (vui64_t) jd0, 0);
+#else
+  jd = (vf64_t) vec_permdi ((vui64_t) jd0, (vui64_t) jd1, 0);
+#endif
+  rc += check_v2f64 ("vec_vlxsspx2 :", (vf64_t) jd, (vf64_t) ed);
+
+  // This test replicates the results of the last 3 tests in single op.
+  ed =  (vf64_t) { 1.0, 15.0 };
+  jd = vec_vglfsso (test_f32, 4, 60);
+
+  rc += check_v2f64 ("vec_vglfsso :", (vf64_t) jd, (vf64_t) ed);
+
+  // This test replicates the results of the last tests with vector offsets.
+  id1 = (vi64_t)  {  4, 60 };
+  jd = vec_vglfsdo (test_f32, id1);
+
+  rc += check_v2f64 ("vec_vglfsdo :", (vf64_t) jd, (vf64_t) ed);
+
+  // This test replicates the results of the last tests with vector indexes.
+  id1 = (vi64_t)  {  1, 15 };
+  jd = vec_vglfsdx (test_f32, id1);
+
+  rc += check_v2f64 ("vec_vglfsdx :", (vf64_t) jd, (vf64_t) ed);
+
+  // This test replicates the results of the last tests with vector
+  // scaled indexes.
+  id1 = (vi64_t)  { 1, 3 };
+  ed =  (vf64_t) { 4.0, 12.0 };
+  jd = vec_vglfsdsx (test_f32, id1, 2);
+
+  rc += check_v2f64 ("vec_vglfsdsx :", (vf64_t) jd, (vf64_t) ed);
+
+
+  // This test replicates the results of the last 3 tests in single op.
+  e =  (vf32_t) { 1.0, 15.0, 2.0, -2.0 };
+  j = vec_vgl4fsso (test_f32, 4, 60, 8, 72);
+
+  rc += check_v4f32 ("vec_vgl4fsso :", j, e);
+
+  // This test replicates the results of the last 3 tests in single op.
+  i1 = (vi32_t) { 4, 60, 8, 72 };
+  e =  (vf32_t) { 1.0, 15.0, 2.0, -2.0 };
+  j = vec_vgl4fswo (test_f32, i1);
+
+  rc += check_v4f32 ("vec_vgl4fswo :", j, e);
+
+  // This test replicates the results of the last tests with indexed op.
+  i1 = (vi32_t) { 1, 15, 2, 18 };
+  e =  (vf32_t) { 1.0, 15.0, 2.0, -2.0 };
+  j = vec_vgl4fswx (test_f32, i1);
+
+  rc += check_v4f32 ("vec_vgl4fswx :", j, e);
+
+  // This test replicates the results of the last tests with scaled indexed op.
+  i1 = (vi32_t) { 1, 15, 2, 9 };
+  e =  (vf32_t) { 2.0, -14.0, 4.0, -2.0 };
+  j = vec_vgl4fswsx (test_f32, i1, 1);
+
+  rc += check_v4f32 ("vec_vgl4fswsx :", j, e);
+  return (rc);
+}
+
+static float test_stf32[16];
+
+int
+test_stvgfsx (void)
+{
+  vi32_t i1, i2;
+  vf32_t e, *mem;
+  vf32_t j, j1;
+  vf64_t jd, jd1, jd2;
+  vi64_t id1;
+  int rc = 0;
+  int i;
+
+  mem = (vf32_t *)& test_stf32;
+
+  for (i=0; i<16; i++)
+    test_stf32[i] = test_f32[i];
+
+  printf ("\ntest_Vector Scatter-Store Single Float\n");
+
+  jd1 = (vf64_t) CONST_VINT64_DW ( 16.0, 1616.0 );
+  jd2 = (vf64_t) CONST_VINT64_DW ( 31.0, 3131.0 );
+  vec_vstxsspx (jd1, 0, test_stf32);
+  vec_vstxsspx (jd2, 60, test_stf32);
+
+  j = mem [0];
+  e = (vf32_t) { 16.0, 1.0, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vstxsspx 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 14.0, 31.0 };
+
+  rc += check_v4f32 ("vec_vstxsspx 2:", j, e);
+
+  jd = (vf64_t) { 0.5, 31.5 };
+  vec_vsstfsso (jd, test_stf32, 0, 60);
+
+  j = mem [0];
+  e = (vf32_t) { 0.5, 1.0, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsstfsso 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 14.0, 31.5 };
+
+  rc += check_v4f32 ("vec_vsstfsso 2:", j, e);
+
+  jd = (vf64_t) { 0.75, 31.75 };
+  id1 = (vi64_t) { 0, 60 };
+  vec_vsstfsdo (jd, test_stf32, id1);
+
+  j = mem [0];
+  e = (vf32_t) { 0.75, 1.0, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsstfsdo 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 14.0, 31.75 };
+
+  rc += check_v4f32 ("vec_vsstfsdo 2:", j, e);
+
+  jd = (vf64_t) { 0.25, 31.25 };
+  id1 = (vi64_t) { 0, 15 };
+  vec_vsstfsdx (jd, test_stf32, id1);
+
+  j = mem [0];
+  e = (vf32_t) { 0.25, 1.0, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsstfsdx 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 14.0, 31.25 };
+
+  rc += check_v4f32 ("vec_vsstfsdx 2:", j, e);
+
+  jd = (vf64_t) { 0.125, 31.125 };
+  id1 = (vi64_t) { 0, 15 };
+  vec_vsstfsdsx (jd, test_stf32, id1, 0);
+
+  j = mem [0];
+  e = (vf32_t) { 0.125, 1.0, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsstfsdsx 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 14.0, 31.125 };
+
+  rc += check_v4f32 ("vec_vsstfsdsx 2:", j, e);
+
+
+  j1 = (vf32_t) { 16.0, 31.0, 17.0, 30.0 };
+  vec_vsst4fsso (j1, test_stf32, 0, 60, 4, 56);
+
+  j = mem [0];
+  e = (vf32_t) { 16.0, 17.0, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsst4fsso 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 30.0, 31.0 };
+
+  rc += check_v4f32 ("vec_vsst4fsso 2:", j, e);
+
+
+  j1 = (vf32_t) { 16.5, 31.5, 17.5, 30.5 };
+  i1 = (vi32_t) { 0, 60, 4, 56 };
+  vec_vsst4fswo (j1, test_stf32, i1);
+
+  j = mem [0];
+  e = (vf32_t) { 16.5, 17.5, 2.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsst4fswo 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 30.5, 31.5 };
+
+  rc += check_v4f32 ("vec_vsst4fswo 2:", j, e);
+
+  j1 = (vf32_t) { 16.75, 31.75, 17.75, 30.75 };
+  i2 = (vi32_t) { 0, 15, 1, 14 };
+  vec_vsst4fswx (j1, test_stf32, i2);
+
+  j = mem [0];
+  e = (vf32_t) { 16.75, 17.75, 2.0, 3.0 };
+
+
+  rc += check_v4f32 ("vec_vsst4fswx 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 12.0, 13.0, 30.75, 31.75 };
+
+  rc += check_v4f32 ("vec_vsst4fswx 2:", j, e);
+
+  j1 = (vf32_t) { 0.0, 7.0, 1.0, 6.0 };
+  i2 = (vi32_t) { 0, 7, 1, 6 };
+  vec_vsst4fswsx (j1, test_stf32, i2, 1);
+
+  j = mem [0];
+  e = (vf32_t) { 0.0, 17.75, 1.0, 3.0 };
+
+  rc += check_v4f32 ("vec_vsst4fswsx 1:", j, e);
+
+  j = mem [3];
+  e = (vf32_t) { 6.0, 13.0, 7.0, 31.75 };
+
+  rc += check_v4f32 ("vec_vsst4fswsx 2:", j, e);
+
+  return (rc);
+}
+
+float matrix_f32 [MN][MN] __attribute__ ((aligned (128)));
+
+void
+test_f32_Imatrix_init (float * array)
+{
+  long i, j, k;
+  long rows, columns;
+
+  rows = columns = MN;
+
+#ifdef __DEBUG_PRINT__
+  printf ("init_indentity array[%d,%d]\n",
+	  rows, columns);
+#endif
+
+  for ( i=0; i<rows; i++ )
+  {
+    for ( j=0; j<columns; j++ )
+      {
+	k = (i * columns) + j;
+	if (i == j)
+	  {
+	    array [k] = 1.0;
+#ifdef __DEBUG_PRINT__
+	    printf ("init_indentity array[%d,%d] is %f\n",
+			i, j, array [k]);
+#endif
+	  }
+	else
+	  {
+	    array [k] = 0.0;
+	  }
+      }
+  }
+}
+
+int
+#if !defined(__clang__)
+__attribute__ ((optimize ("unroll-loops")))
+#endif
+test_f32_Imatrix_check (float * array)
+{
+  long i, j, k;
+  long rows, columns;
+  int rc = 0;
+
+  rows = columns = MN;
+
+  for ( i=0; i<rows; i++ )
+  {
+    for ( j=0; j<columns; j++ )
+      {
+	k = (i * columns) + j;
+	if (i == j)
+	  {
+	    if ( array [k] != 1.0 )
+	      {
+		printf ("check_indentity array[%ld,%ld] !=1.0 is %f\n",
+			i, j, array [k]);
+		rc++;
+	      }
+	  }
+	else
+	  {
+	    if ( array [k] != 0.0 )
+	      {
+		printf ("check_indentity array[%ld,%ld] !=0.0 is %f\n",
+			i, j, array [k]);
+		rc++;
+	      }
+	  }
+      }
+  }
+  if (rc)
+    {
+      printf ("check_indentity array failed rc=%d\n",
+		rc);
+
+    }
+  return rc;
+}
+
+void
+#if !defined(__clang__)
+__attribute__ ((optimize ("unroll-loops")))
+#endif
+test_f32_matrix_transpose (float * tm, float * m)
+{
+  long i, j, k, l;
+  long rows, columns;
+
+  rows = columns = MN;
+
+  for ( i=0; i<rows; i++ )
+  {
+    for ( j=0; j<columns; j++ )
+      {
+	k = (i * columns) + j;
+	l = (j * columns) + i;
+	tm[l] = m[k];
+      }
+  }
+}
+
+void
+//__attribute__ ((optimize ("unroll-loops")))
+test_f32_matrix_gather_transpose (float * tm, float * m)
+{
+  vi32_t vra_init = { 0, MN*4, (MN*2)*4, (MN*3)*4 };
+  vi32_t vra;
+  vi32_t stride = { MN * 4 * 4, MN * 4 * 4, MN * 4 * 4, MN * 4 * 4 };
+  long i, j;
+  long rows, columns;
+
+  rows = columns = MN;
+  vra = vra_init;
+#ifdef __DEBUG_PRINT__
+  printf ("test_f32_matrix_gather_transpose (%p, %p)\n", tm, m);
+  printf ("vra    = {%d, %d, %d, %d}\n",
+	  vra[0], vra[1], vra[2], vra[3]);
+  printf ("stride = {%d, %d, %d, %d}\n",
+	  stride[0], stride[1], stride[2], stride[3]);
+#endif
+  for (i = 0; i < rows; i++)
+    {
+      float *cadr = &m[i];
+      vf32_t *radr = (vf32_t*)&tm[(i * columns)];
+      vra = vra_init;
+      for (j = 0; j < columns/4; j++)
+	{
+	  radr[j] = vec_vgl4fswo (cadr, vra);
+	  vra = vec_add ( vra, stride);
+	}
+    }
+}
+
+void
+//__attribute__ ((optimize ("unroll-loops")))
+test_f32_matrix_gatherx2_transpose (float * tm, float * m)
+{
+  vi32_t vra_init = { 0, MN*4, (MN*2)*4, (MN*3)*4 };
+  vi32_t vra;
+  vi32_t stride = { MN * 4 * 4, MN * 4 * 4, MN * 4 * 4, MN * 4 * 4 };
+  long i, j;
+  long rows, columns;
+
+  rows = columns = MN;
+
+  for (i = 0; i < rows; i+=2)
+    {
+      float *cadr = &m[i];
+      float *cadr1 = &m[i+1];
+      vf32_t *radr = (vf32_t*)&tm[(i * columns)];
+      vf32_t *radr1 = (vf32_t*)&tm[((i+1) * columns)];
+
+      vra = vra_init;
+      for (j = 0; j < columns/4; j++)
+	{
+	  vf32_t vrow0, vrow1;
+	  vrow0 = vec_vgl4fswo (cadr, vra);
+	  vrow1 = vec_vgl4fswo (cadr1, vra);
+	  radr[j] = vrow0;
+	  radr1[j] = vrow1;
+	  vra = vec_add ( vra, stride);
+	}
+    }
+}
+
+#if 1
+void
+//__attribute__ ((optimize ("unroll-loops")))
+test_f32_matrix_gatherx4_transpose (float * tm, float * m)
+{
+  vi32_t vra_init = { 0, MN*4, (MN*2)*4, (MN*3)*4 };
+  vi32_t vra;
+  vi32_t stride = { MN * 4 * 4, MN * 4 * 4, MN * 4 * 4, MN * 4 * 4 };
+  long i, j;
+  long rows, columns;
+
+  rows = columns = MN;
+
+  for (i = 0; i < rows; i+=4)
+    {
+      float *cadr = &m[i];
+      float *cadr1 = &m[i+1];
+      float *cadr2 = &m[i+2];
+      float *cadr3 = &m[i+3];
+      vf32_t *radr = (vf32_t*)&tm[(i * columns)];
+      vf32_t *radr1 = (vf32_t*)&tm[((i+1) * columns)];
+      vf32_t *radr2 = (vf32_t*)&tm[((i+2) * columns)];
+      vf32_t *radr3 = (vf32_t*)&tm[((i+3) * columns)];
+
+      vra = vra_init;
+      for (j = 0; j < columns/4; j++)
+	{
+	  vf32_t vrow0, vrow1, vrow2, vrow3;
+	  vrow0 = vec_vgl4fswo (cadr, vra);
+	  vrow1 = vec_vgl4fswo (cadr1, vra);
+	  vrow2 = vec_vgl4fswo (cadr2, vra);
+	  vrow3 = vec_vgl4fswo (cadr3, vra);
+	  radr[j] = vrow0;
+	  radr1[j] = vrow1;
+	  radr2[j] = vrow2;
+	  radr3[j] = vrow3;
+	  vra = vec_add ( vra, stride);
+	}
+    }
+}
+#endif
+int
+test_f32_indentity_array ()
+{
+  float tmatrix[MN][MN] __attribute__ ((aligned (128)));
+  int rc = 0;
+
+  printf ("\ntest_indentity_array\n");
+
+  test_f32_Imatrix_init  (&matrix_f32[0][0]);
+
+  rc += test_f32_Imatrix_check (&matrix_f32[0][0]);
+
+  test_f32_matrix_transpose (&tmatrix[0][0], &matrix_f32[0][0]);
+
+  rc += test_f32_Imatrix_check (&tmatrix[0][0]);
+
+  test_f32_matrix_gather_transpose (&tmatrix[0][0], &matrix_f32[0][0]);
+
+  rc += test_f32_Imatrix_check (&tmatrix[0][0]);
+
+  test_f32_matrix_gatherx2_transpose (&tmatrix[0][0], &matrix_f32[0][0]);
+
+  rc += test_f32_Imatrix_check (&tmatrix[0][0]);
+
+#if 1
+  test_f32_matrix_gatherx4_transpose (&tmatrix[0][0], &matrix_f32[0][0]);
+
+  rc += test_f32_Imatrix_check (&tmatrix[0][0]);
+#endif
+  return rc;
+}
 
 #define TIMING_ITERATIONS 10
 
@@ -1835,6 +2343,65 @@ test_time_f32 (void)
   printf ("\n%s fpclassify_f32  tb delta = %lu, sec = %10.6g\n", __FUNCTION__,
 	  t_delta, delta_sec);
 
+  // initialize the test array to the Identity matrix
+  test_f32_Imatrix_init  (&matrix_f32[0][0]);
+
+  printf ("\n%s scalar_transpose_f32 start, ...\n", __FUNCTION__);
+  t_start = __builtin_ppc_get_timebase ();
+  for (i = 0; i < TIMING_ITERATIONS; i++)
+    {
+      rc += timed_scalar_f32_transpose ();
+    }
+  t_end = __builtin_ppc_get_timebase ();
+  t_delta = t_end - t_start;
+  delta_sec = TimeDeltaSec (t_delta);
+
+  printf ("\n%s scalar_transpose_f32 end", __FUNCTION__);
+  printf ("\n%s scalar_transpose_f32  tb delta = %lu, sec = %10.6g\n", __FUNCTION__,
+	  t_delta, delta_sec);
+
+  printf ("\n%s gather_transpose_f32 start, ...\n", __FUNCTION__);
+  t_start = __builtin_ppc_get_timebase ();
+  for (i = 0; i < TIMING_ITERATIONS; i++)
+    {
+      rc += timed_gather_f32_transpose ();
+    }
+  t_end = __builtin_ppc_get_timebase ();
+  t_delta = t_end - t_start;
+  delta_sec = TimeDeltaSec (t_delta);
+
+  printf ("\n%s gather_transpose_f32 end", __FUNCTION__);
+  printf ("\n%s gather_transpose_f32  tb delta = %lu, sec = %10.6g\n", __FUNCTION__,
+	  t_delta, delta_sec);
+
+  printf ("\n%s gatherx2_transpose_f32 start, ...\n", __FUNCTION__);
+  t_start = __builtin_ppc_get_timebase ();
+  for (i = 0; i < TIMING_ITERATIONS; i++)
+    {
+      rc += timed_gatherx2_f32_transpose ();
+    }
+  t_end = __builtin_ppc_get_timebase ();
+  t_delta = t_end - t_start;
+  delta_sec = TimeDeltaSec (t_delta);
+
+  printf ("\n%s gatherx2_transpose_f32 end", __FUNCTION__);
+  printf ("\n%s gatherx2_transpose_f32  tb delta = %lu, sec = %10.6g\n", __FUNCTION__,
+	  t_delta, delta_sec);
+
+  printf ("\n%s gatherx4_transpose_f32 start, ...\n", __FUNCTION__);
+  t_start = __builtin_ppc_get_timebase ();
+  for (i = 0; i < TIMING_ITERATIONS; i++)
+    {
+      rc += timed_gatherx4_f32_transpose ();
+    }
+  t_end = __builtin_ppc_get_timebase ();
+  t_delta = t_end - t_start;
+  delta_sec = TimeDeltaSec (t_delta);
+
+  printf ("\n%s gatherx4_transpose_f32 end", __FUNCTION__);
+  printf ("\n%s gatherx4_transpose_f32  tb delta = %lu, sec = %10.6g\n", __FUNCTION__,
+	  t_delta, delta_sec);
+
   return (rc);
 }
 
@@ -1855,6 +2422,9 @@ test_vec_f32 (void)
   rc += test_float_issubnormal ();
   rc += test_float_iszero ();
   rc += test_float_isfinite ();
+  rc += test_lvgfsx ();
+  rc += test_stvgfsx ();
+  rc += test_f32_indentity_array ();
 
   rc += test_time_f32 ();
 
